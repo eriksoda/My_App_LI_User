4825dd0a77c1c958b2b7f41b5317318403844510
eriksoda <117855510+eriksoda@users.noreply.github.com>
2022-12-13 08:55:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 
 st.markdown("# Are You LinkedIn?")
